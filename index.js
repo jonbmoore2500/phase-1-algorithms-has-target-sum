25a4fa486b1e37e5c2393cc4323463f9ccd050ea
Jonathan <jonathanmoorevla@gmail.com>
2022-07-17 21:48:33 -0500

    commit tree 7aba5410f6ab38c6cb6f604a1f4739d60137c43c

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let element = 0; element < array.length; element++) {
+    debugger
+    let matchValue = target - array[element]
+    for (let element2 = element+1; element2 < array.length; element2++) {
+      if (array[element2] === matchValue) {
+        return true
+      }
+    }
+  }
+  return false
 }
+console.log('test', hasTargetSum([[1, 2, 5], 4]))
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +19,19 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+ 
+
+
 */
 
 /*
   Add written explanation of your solution here
+  
+  Could try to remove values higher than the target, but future uses could have negatives and it needs an extra iteration pass without saving that much
+   2 options, similar complexities
+  Option 1: Iterate through array, adding number to every number after and seeing if it matches target. O(n^2) - nested iteration
+  Option 2: Iterate through array, subtracting number from target and seeing if result exists in remainder of array. O(n^2) - nested iteration
+    Option 2.1: Iterate through array, subtracting number from target and seeking result. Since result is known, can order array and discarding halves that are less than the target 
 */
 
 // You can run `node index.js` to view these console logs
